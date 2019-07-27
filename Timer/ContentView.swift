517c4c30dce622adff32e3af0ec6303adfed9466
kazuki <honeybee.zzz119@gmail.com>
2019-07-27 22:42:51 +0900

    Initial Commit

---
+++ Timer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Timer
+//
+//  Created by 下澤一輝 on 2019/07/27.
+//  Copyright © 2019 下澤一輝. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
